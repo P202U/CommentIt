2bcf1ab883f10ae19d4ed2466692f597a1815a0a
P202U <156369604+P202U@users.noreply.github.com.>
2025-03-30 19:23:51 +0530

    INIT

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="text-2xl font-bold ">
+      Hello world!
+    </div>
+  )
+}
+
+export default App
---
+++ client/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+    },
+  },
+});
